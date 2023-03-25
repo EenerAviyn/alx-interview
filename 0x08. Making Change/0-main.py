2bd70be71238e0706b420acd68be41303281bdb3
Renée Mulela <42552984+EenerAviyn@users.noreply.github.com>
2023-03-25 22:49:59 +0300

    0-main.py

--- 0x08. Making Change/0-main.py
+++ 0x08. Making Change/0-main.py
@@ -1,15 +1,10 @@
 #!/usr/bin/python3
 """
-Main File for testing
+Main file for testing
 """
 
 makeChange = __import__('0-making_change').makeChange
 
 print(makeChange([1, 2, 25], 37))
-"""
-1    2    25
--------------
-1    6    1
-"""
 
 print(makeChange([1256, 54, 48, 16, 102], 1453))
